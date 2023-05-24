dc1a755162bfaf059d83278cf980c0d911150335
YASH TAILOR <theyash.tailor@gmail.com>
2023-05-24 23:18:02 +0530

    Added : Exception practical

---
+++ src/com/yash/exception/Main.java
@@ -0,0 +1,27 @@
+package com.yash.exception;
+
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter first Number : ");
+        int num1 = scn.nextInt();
+        System.out.print("Enter Second Number : ");
+        int num2 = scn.nextInt();
+        try{
+            System.out.println("Division of given two numbers is :" + (num1/num2));
+        }catch (ArithmeticException e){
+            e.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if(num2 == 0){
+                System.err.println("Can't divide by zero");
+            }else{
+                System.out.println("Division Successfully Completed!");
+            }
+        }
+    }
+}
