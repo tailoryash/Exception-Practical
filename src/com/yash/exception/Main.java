8c9a8d40963f870ed667cdff24780033a42553fd
YASH TAILOR <theyash.tailor@gmail.com>
2023-05-26 20:30:25 +0530

    formatted code

--- src/com/yash/exception/Main.java
+++ src/com/yash/exception/Main.java
@@ -1,4 +1,5 @@
 package com.yash.exception;
+
 import java.util.Scanner;
 
 public class Main {
@@ -8,16 +9,16 @@ public class Main {
         int num1 = scn.nextInt();
         System.out.print("Enter Second Number : ");
         int num2 = scn.nextInt();
-        try{
-            System.out.println("Division of given two numbers is :" + (num1/num2));
-        }catch (ArithmeticException e){
+        try {
+            System.out.println("Division of given two numbers is :" + (num1 / num2));
+        } catch (ArithmeticException e) {
             e.printStackTrace();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
-            if(num2 == 0){
+        } finally {
+            if (num2 == 0) {
                 System.err.println("Can't divide by zero");
-            }else{
+            } else {
                 System.out.println("Division Successfully Completed!");
             }
         }
